942b2100253fe7a7c1c3c57004893e1a613e6e1f
maiwenchrist <maiwen.christ@efrei.net>
2023-12-21 08:18:09 +0100

    part two done

--- functions.py
+++ functions.py
@@ -3,8 +3,10 @@ import string
 import math
 from collections import defaultdict
 
-
+# PARTIE 1
 #  Les fonctions de base
+
+
 def list_of_files(directory, extension):
     files_names = []
     for filename in os.listdir(directory):
@@ -30,8 +32,24 @@ def extract_presidents(files_names):
     return list(presidents)
 
 
+def associate_president_names():
+    president_names = {
+        "Giscard dEstaing": 'Valéry',
+        "Mitterand": 'François',
+        "Chirac": 'Jacque',
+        "Sarkozy": 'Nicolas',
+        "Hollande": 'François',
+        "Macron": 'Emmanuel'
+    }
+
+    return president_names
+
+
 # demander au prof
 def convert_text(directory, cleaned_directory):
+    # Vérification de la création du dossier cleaned
+    os.makedirs(cleaned_directory, exist_ok=True)
+
     files_names = list_of_files(directory, "txt")
 
     for filename in files_names:
@@ -44,8 +62,8 @@ def convert_text(directory, cleaned_directory):
             # Écriture du texte nettoyé dans un nouveau fichier
             cleaned_filename = os.path.join(cleaned_directory, filename)
 
-        with open(cleaned_filename, 'w', encoding='utf-8') as file:
-            file.write(text)
+            with open(cleaned_filename, 'w', encoding='utf-8') as cleaned_file:
+                cleaned_file.write(text)
 
 
 # La methode TF-IDF
@@ -75,7 +93,7 @@ def calculate_idf(directory):
 
     idf_scores = {}
     for word, count in word_doc_count.items():
-        idf = round(math.log(num_docs / count))
+        idf = (math.log10(num_docs / count))
         idf_scores[word] = idf
 
     return idf_scores
@@ -98,14 +116,14 @@ def calculate_tf_idf(directory):
                     tf = word_freq[word] if word in word_freq else 0
                     tfidf = tf * word_idf[word]
                     # Convertir en entier
-                    tfidf_vector.append(int(tfidf))  # Convertir en entier ici
+                    tfidf_vector.append(float(round(tfidf, 2)))  # Convertir en réel ici
 
                 tfidf_matrix.append(tfidf_vector)
 
     return tfidf_matrix
 
 
-"""def non_important_words(tfidf_matrix, unique_words):
+def non_important_words(tfidf_matrix, unique_words):
     non_important = []
     for i, word in enumerate(unique_words):
         word_tfidf = [doc[i] for doc in tfidf_matrix]
@@ -115,22 +133,96 @@ def calculate_tf_idf(directory):
 
 
 def highest_tfidf_words(corpus_directory):
-
     tf_idf_matrix = calculate_tf_idf(corpus_directory)
+    max_tfidf_words = []
 
-    mots_moins_importants = []
     for i, doc in enumerate(tf_idf_matrix):
-        if all(score == 0 for score in doc):
-            mots_moins_importants.append(i)
+        max_tfidf = max(doc)
+        if max_tfidf > 0:
+            max_tfidf_indices = [index for index, value in enumerate(doc) if value == max_tfidf]
+            max_tfidf_words.extend(max_tfidf_indices)
 
-    return mots_moins_importants
+    return max_tfidf_words
 
 
+# Fonction pour obtenir les mots à partir des indices
+def get_words_from_indices(indices, unique_words):
+    return [unique_words[idx] for idx in indices]
 
-def display_non_important_words(tfidf_matrix, unique_words):
-    non_important = non_important_words(tfidf_matrix, unique_words)
-    if non_important:
-        print("Mots non importants (avec un score TF-IDF de 0 dans tous les fichiers):")
-        print(", ".join(non_important))
+
+def highest_words_bychirac(corpus_directory):
+    tf_idf_matrix = calculate_tf_idf(corpus_directory)
+    non_important = non_important_words(tf_idf_matrix, list(calculate_idf(corpus_directory).keys()))
+    max_tdidf_chirac = defaultdict(float)
+
+    for i, doc in enumerate(tf_idf_matrix):
+        if "Chirac" in list_of_files(corpus_directory, ".txt")[i]:  # Vérifie si le fichier correspond à Chirac
+            for index, value in enumerate(doc):
+                if value > 0 and index not in non_important:
+                    max_tdidf_chirac[index] += value
+
+    # Obtenez l'indice du mot le plus répété pour Chirac
+    if max_tdidf_chirac:
+        max_index = max(max_tdidf_chirac, key=max_tdidf_chirac.get)
+        return max_index
     else:
-        print("Aucun mot non important trouvé.")"""
+        return None
+
+
+def words_nation(corpus_directory):
+    tf_idf_matrix = calculate_tf_idf(corpus_directory)
+    unique_words = list(calculate_idf(corpus_directory).keys())
+    word_indices = [i for i, word in enumerate(unique_words) if word.lower() == 'nation']
+
+    if word_indices:
+        president_word_count = defaultdict(float)
+        presidents_with_nation = set()  # Utilisation d'un ensemble pour éviter les doublons
+
+        for i, doc in enumerate(tf_idf_matrix):
+            filename = list_of_files(corpus_directory, ".txt")[i]
+            president_name = filename.split("_")[1].capitalize()
+            for index in word_indices:
+                president_word_count[president_name] += doc[index]
+                if doc[index] > 0:
+                    presidents_with_nation.add(president_name)
+
+        if president_word_count:
+            max_repeated_president = max(president_word_count, key=president_word_count.get)
+
+            return {
+                "Presidents": list(presidents_with_nation),
+                "Most Repeated President": max_repeated_president,
+                "Word Count": president_word_count[max_repeated_president]
+            }
+    return None
+
+
+def words_environment(corpus_directory):
+    tf_idf_matrix = calculate_tf_idf(corpus_directory)
+    unique_words = list(calculate_idf(corpus_directory).keys())
+
+    # Chercher les mots liés à l'environnement, par exemple : climat, écologie, environnement, etc.
+    environment_words = ['climat', 'écologie', 'environnement']  # Vous pouvez ajouter d'autres mots-clés
+
+    word_indices = [i for i, word in enumerate(unique_words) if word.lower() in environment_words]
+
+    if word_indices:
+        president_word_count = defaultdict(float)
+        presidents_about_environment = set()
+
+        for i, doc in enumerate(tf_idf_matrix):
+            filename = list_of_files(corpus_directory, ".txt")[i]
+            president_name = filename.split("_")[1].capitalize()
+            for index in word_indices:
+                president_word_count[president_name] += doc[index]
+                if doc[index] > 0:
+                    presidents_about_environment.add(president_name)
+
+        if president_word_count:
+            return {
+                "Presidents": list(presidents_about_environment),
+                "Word Count": president_word_count
+            }
+    return None
+
+# PARTIE 2 dans functions2
--- main.py
+++ main.py
@@ -1,104 +1,35 @@
 from functions import *
+from functions2 import *
 
-
-"""print()
-print("//*** Voici l'extraction des noms de présidents à partir des fichiers texte ***//")
 print()
-
-directory = "./speeches"
-files_list = list_of_files(directory, ".txt")
-print_list(files_list)
-print()
-
-presidents_list = extract_presidents(files_list)
-print("//*** Noms de fichier ***//")
-print()
-print(presidents_list)
-print()
-
-# dictionnaire
-names = {
-    "Giscard dEstaing": 'Valéry',
-    "Mitterand": 'François',
-    "Chirac": 'Jacque',
-    "Sarkozy": 'Nicolas',
-    "Hollande": 'François',
-    "Macron": 'Emmanuel'
-}
-print("//*** Prénoms associer au nom de presidents sans doublon ***//")
-print()
-
-# affiche des noms et prenoms des presidents
-for cle, name_president in names.items():
-    print(cle, ':', name_president)
-print()
-
-cleaned_directory = "./cleaned"
-convert_text(directory, cleaned_directory)
-print("//*** Fichier nettoyés succès ***//")
-print()
-
-print("//*** Frequence des termes ***//")
-print()
-
-cleaned_files_list = list_of_files(cleaned_directory, ".txt")
-
-for filename in cleaned_files_list:
-    with open(os.path.join(cleaned_directory, filename), 'r', encoding='utf-8') as file:
-        text = file.read()
-        word_counts = counter(text)
-
-        # Affichage des occurrences de mots pour chaque fichier
-        print(f"Occurrences de mots dans le fichier {filename}:")
-        print()
-        for word, count in word_counts.items():
-            print(f"Le mot '{word}' apparaît {count} fois.")
-        print()
-
-
-print("//*** Fréquence Inverse du Document ***//")
-print()
-
-corpus_directory = "./cleaned"
-idf_scores = calculate_idf(corpus_directory)
-
-# Affichage des scores IDF pour chaque mot (arrondis à l'entier le plus proche)
-for word, score in idf_scores.items():
-    print(f"Mot: {word}, Score IDF: {score}")
-print()
-
-tf_idf_matrix = calculate_tf_idf(corpus_directory)
-
-# Affichage de la matrice TF-IDF
-print("Matrice TF-IDF:")
-print()
-
-for row in tf_idf_matrix:
-    print(row)
-
-"""
-
-print("Bienvenue dans le programme principal !")
+print("\t\tBienvenue dans le programme principal !")
 while True:
-    print("\nMenu :")
-    print("1. Afficher les noms de fichiers")
-    print("2. Extraire les noms de présidents")
-    print("3. Nettoyer les fichiers")
-    print("4. Afficher la fréquence des termes")
-    print("5. Afficher la fréquence inverse du document")
-    print("6. Afficher la matrice TF-IDF")
-    print("7. Afficher les mots les moins importants")
-    print("8. Afficher les mots avec le score TF-IDF le plus élevé")
-    print("0. Quitter")
+    print()
+    print("\t\t\t\t\t\tMenu ")
+    print()
+    print("\t\t1. Afficher les noms de fichiers")
+    print("\t\t2. Extraire les noms de présidents")
+    print("\t\t3. Nettoyer les fichiers")
+    print("\t\t4. Afficher la fréquence des termes")
+    print("\t\t5. Afficher la fréquence inverse du document")
+    print("\t\t6. Afficher la matrice TF-IDF")
+    print("\t\t7. Afficher les mots les moins importants")
+    print("\t\t8. Afficher les mots avec le score TF-IDF le plus élevé")
+    print("\t\t9. Afficher le(s) mot(s) le(s) plus répété(s) par Chirac")
+    print("\t\t10. Afficher le(s) nom(s) du (des) président(s) qui a (ont) parlé de la « Nation » et celui qui l’a répété le plus de fois ")
+    print("\t\t11. Afficher le(s) nom(s) président(s) qui a (ont) parlé de la 'Nation'  et celui qui l'a répété le plus de fois ")
+    print("\t\t12. ")
+    print("\t\t0. Quitter")
+    print()
 
-    choix = input("Veuillez sélectionner une option (0-7) : ")
+    choix = input("Veuillez sélectionner une option (0-13) : ")
+    print()
 
     if choix == "1":
         print("//*** Affichage des noms de fichiers ***//")
         files_list = list_of_files("./speeches", ".txt")
         print_list(files_list)
 
-    elif choix == "2":
         print("//*** Extraction des noms de présidents ***//")
         files_list = list_of_files("./speeches", ".txt")
 
@@ -107,30 +38,22 @@ while True:
         print("Noms des présidents :")
         print(presidents_list)
 
-    elif choix == "3":
-        names = {
-            "Giscard dEstaing": 'Valéry',
-            "Mitterand": 'François',
-            "Chirac": 'Jacque',
-            "Sarkozy": 'Nicolas',
-            "Hollande": 'François',
-            "Macron": 'Emmanuel'
-        }
+    elif choix == "2":
         print("//*** Prénoms associer au nom de presidents sans doublon ***//")
         print()
 
-        # affiche des noms et prenoms des presidents
-        for cle, name_president in names.items():
+        nom_president = associate_president_names()
+        for cle, name_president in nom_president.items():
             print(cle, ':', name_president)
         print()
 
-    elif choix == "4":
+    elif choix == "3":
         print("//*** Nettoyage des fichiers ***//")
         print()
         convert_text("./speeches", "./cleaned")
         print("Fichiers nettoyés avec succès !")
 
-    elif choix == "5":
+    elif choix == "4":
         cleaned_directory = "./cleaned"
         print("//*** Fréquence des termes ***//")
         print()
@@ -149,7 +72,7 @@ while True:
                     print(f"Le mot '{word}' apparaît {count} fois.")
                 print()
 
-    elif choix == "6":
+    elif choix == "5":
         print("//*** Fréquence Inverse du Document ***//")
         print()
 
@@ -161,16 +84,108 @@ while True:
             print(f"Mot: {word}, Score IDF: {score}")
         print()
 
-    elif choix == "7":
+    elif choix == "6":
         corpus_directory = "./cleaned"
         tf_idf_matrix = calculate_tf_idf(corpus_directory)
 
         # Affichage de la matrice TF-IDF
-        print("Matrice TF-IDF:")
+        print("//*** Matrice TF-IDF ***//")
         print()
 
         for row in tf_idf_matrix:
             print(row)
+
+    elif choix == "7":
+        print("//*** Liste des mots les moins important")
+        print()
+
+        corpus_directory = "./cleaned"
+        tf_idf_matrix = calculate_tf_idf(corpus_directory)
+        idf_scores = calculate_idf(corpus_directory)
+
+        mots_non_importants = non_important_words(tf_idf_matrix, list(idf_scores.keys()))
+
+        print(mots_non_importants)
+
+    elif choix == "8":
+        print("//*** Mots ayant un score TD-IDF le plus élevé")
+        print()
+
+        corpus_directory = "./cleaned"
+        mots_importants_indices = highest_tfidf_words(corpus_directory)
+
+        unique_words = list(calculate_idf(corpus_directory).keys())
+        mots_importants = get_words_from_indices(mots_importants_indices, unique_words)
+
+        if mots_importants:
+            print("Mots avec le score TF-IDF le plus élevé :", mots_importants)
+        else:
+            print("Aucun mot trouvé avec un score TF-IDF élevé.")
+
+    elif choix == '9':
+        print("//*** Mots les plus répétés par Chirac ***//")
+        print()
+
+        corpus_directory = "./cleaned"  # Adapter le répertoire selon votre structure
+        highest_words_by_chirac = highest_words_bychirac(corpus_directory)
+
+        if highest_words_by_chirac is not None:
+            unique_words = list(calculate_idf(corpus_directory).keys())
+            chirac_word = get_words_from_indices([highest_words_by_chirac], unique_words)[0]
+            print(f"Le mot le plus fréquent pour Chirac (hors mots non importants) est : {chirac_word}")
+        else:
+            print("Aucun mot le plus fréquent pour Chirac n'a été trouvé.")
+
+    elif choix == '11':
+        corpus_directory = "./cleaned"
+        result = words_nation(corpus_directory)
+        if result:
+            most_repeated_president = result["Most Repeated President"].replace('.txt', '')
+
+            print("Le(s) président(s) qui a(ont) parlé de la « Nation » :", list(result["Presidents"]))
+            print("Le(s) président(s) qui a(ont) le plus répété le mot « Nation » :", most_repeated_president)
+        else:
+            print("Aucun président n'a parlé de la « Nation » ou le mot n'a pas été trouvé.")
+
+    elif choix == '12':
+        print("//*** Présidents parlant du climat et de l'écologie ***//")
+        print()
+        corpus_directory = "./cleaned"
+        result = words_environment(corpus_directory)
+        if result:
+            print("Le(s) président(s) qui a(ont) parlé du climat et/ou de l’écologie :", list(result["Presidents"]))
+        else:
+            print("Aucun président n'a parlé du climat et/ou de l’écologie ou les mots-clés n'ont pas été trouvés.")
+
+    elif choix == '13':
+        corpus_directory = "./cleaned"
+        tf_idf_matrix = calculate_tf_idf(corpus_directory)
+        idf_scores = calculate_idf(corpus_directory)
+        unique_words = list(calculate_idf(corpus_directory).keys())
+
+        question_user = input("\nPosez votre question: ")
+        tokens = tokenize_question(question_user)
+        terms_in_corpus = find_terms_in_corpus(tokens, idf_scores)
+        question_tf_idf = calculate_question_tf_idf(question_user, unique_words, idf_scores)
+        most_relevant_doc_index = find_most_relevant_document(tf_idf_matrix, question_tf_idf)
+        most_relevant_doc_name = filename[most_relevant_doc_index]
+        with open(os.path.join(corpus_directory, most_relevant_doc_name), 'r', encoding='utf-8') as file:
+            text = file.read()
+        highest_tf_idf_word_in_question = highest_tf_idf_word(question_tf_idf, unique_words)
+        question_starters = {
+            "Comment": "Après analyse, ",
+            "Pourquoi": "Car, ",
+            "Peux-tu": "Oui, bien sûr! ",
+        }
+        relevant_sentence = find_sentence_with_word(text, highest_tf_idf_word_in_question)
+        refined_answer = refine_response(question_user, relevant_sentence)
+        print("Document pertinent:", most_relevant_doc_name)
+        print()
+        print("Mot le plus pertinent dans la question:", highest_tf_idf_word_in_question)
+        print()
+        print("Réponse générée:", relevant_sentence)
+        print()
+        print("Réponse raffinée:", refined_answer)
 
     elif choix == "0":
         print("Au revoir !")
